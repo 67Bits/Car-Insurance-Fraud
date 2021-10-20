597284172f17edb076f25e4c43d451e8554bd6d1
Juannico <juan_nico14@hotmail.com>
2021-10-20 17:03:09 -0500

    eighth pack

---
+++ Assets/MisScripts/CharacterMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * 10 * Time.deltaTime;
+        }   
+    }
+}
+    
