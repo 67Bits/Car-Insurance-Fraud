62146671e18df47532b2be6beafff2342ffb55e3
Juannico <juan_nico14@hotmail.com>
2021-10-20 17:36:06 -0500

    fifteenth pack

---
+++ Assets/RootMotion/PuppetMaster/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystick : Joystick
+{
+    public ControlCanvas control_canvas;
+
+    protected override void Start()
+    {
+        control_canvas = GameObject.FindGameObjectWithTag("controlcanvas").GetComponent<ControlCanvas>();
+
+        base.Start();
+        background.gameObject.SetActive(false);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        control_canvas.iniciarJuego();
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        background.gameObject.SetActive(true);
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        background.gameObject.SetActive(false);
+        base.OnPointerUp(eventData);
+    }
+}
